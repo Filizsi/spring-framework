e325ebbeb65c6163c220d0b15d09d05a4ddae848
Filizsi <filizcoskunuzer@gmail.com>
2022-03-03 20:27:16 -0600

    More practices with Spring MVC

--- spring-framework/Spring-07-Spring-MVC-Controller/src/main/java/com/cydeo/controller/HomeController.java
+++ spring-framework/Spring-07-Spring-MVC-Controller/src/main/java/com/cydeo/controller/HomeController.java
@@ -3,24 +3,23 @@ package com.cydeo.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@Controller// annotates the calss woth the @controller stereotype annotation
+@Controller// annotates the calls with the @controller stereotype annotation
 public class HomeController {
     @RequestMapping("/home")//use @Request mapping annotation to associate with an HTTP request path
-    public String home(){
+    public String home() {
         return "home.html";//return th html document name that contains the details we want the browser to display
     }
 
     @RequestMapping("/welcome")//end point
-    public String home2(){
+    public String home2() {
         return "welcome.html";
     }
 
     //if we don't put any end point default is "/"
     @RequestMapping
-    public String home3(){
+    public String home3() {
         return "welcome.html";
     }
-
 
 
 }
--- spring-framework/Spring-08-Spring-MVC-Model/src/main/java/com/cydeo/controller/StudentController.java
+++ spring-framework/Spring-08-Spring-MVC-Model/src/main/java/com/cydeo/controller/StudentController.java
@@ -1,10 +1,18 @@
 package com.cydeo.controller;
 
+import com.cydeo.enums.Gender;
+import com.cydeo.model.Mentor;
+import com.cydeo.model.Student;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
 @Controller
+@RequestMapping("/student")
 public class StudentController {
 
     @RequestMapping("/welcome")
@@ -15,8 +23,47 @@ public class StudentController {
         model.addAttribute("name","Cydeo");
         model.addAttribute("course","MVC");
 
+        String subject = "Collections";
+        model.addAttribute("subject",subject);
+
+        //create student id,
+        int id = new Random().nextInt();
+        model.addAttribute("id",id);
+
+        //list
+        List<Integer> numbers = new ArrayList<>();
+        numbers.add(3);
+        numbers.add(6);
+        numbers.add(11);
+        model.addAttribute("numbers",numbers);
+
+        Student student = new Student(1,"Mike","Smith");
+        //value is the student object
+        model.addAttribute("student",student);
+
+        //no .html anymore, because this is thymeleaf
         return"/student/welcome";
     }
+
+    @RequestMapping("/list")
+    public String mentor(Model model) {
+
+        Mentor mentor = new Mentor("Mike", "Smith", 45, Gender.MALE);
+        List<Mentor> mentorList = new ArrayList<>();
+        mentorList.add(new Mentor("Mikessss", "Smith", 45, Gender.MALE));
+        mentorList.add(new Mentor("Tom", "Hanks", 65, Gender.MALE));
+        mentorList.add(new Mentor("Ammy", "Bryan", 65, Gender.FEMALE));
+
+        model.addAttribute("mentors", mentorList);
+
+
+        return "/mentor/MentorInfo";
+    }
+
+
+
+
+
 
 
 }
