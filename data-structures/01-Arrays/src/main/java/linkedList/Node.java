850dce4fac564d5a05ee31031821acf3382a4c29
Filizsi <54239501+Filizsi@users.noreply.github.com>
2022-08-09 12:58:47 -0400

    Delete data-structures directory

--- data-structures/01-Arrays/src/main/java/linkedList/Node.java
+++
@@ -1,6 +0,0 @@
-package linkedList;
-
-public class Node {
-    int data;
-    Node next;
-}
--- data-structures/02-LinkedLists/src/main/java/MyNode.java
+++
@@ -1,7 +0,0 @@
-public class MyNode {
-    String name;
-    MyNode next;// default value will be null
-    public MyNode(String name) {
-        this.name = name;
-    }
-}
